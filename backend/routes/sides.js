2c14f6ba4e1662a9f078214b5365f4ffcc748a65
Joseph Sabbagh <joseph544310@gmail.com>
2020-10-30 15:53:01 +0100

    create side route is now nested after debates/:id

--- backend/routes/sides.js
+++ backend/routes/sides.js
@@ -1,23 +1,32 @@
 const express = require("express");
 const router  = express.Router({mergeParams: true});
 const Side = require('../models/side');
+const Debate = require('../models/debate');
 
-//create side and send back its id
-router.post('/create', async (req, res) => {
-    try {
+//create side
+router.post('/', async (req, res) => {
+    
+    // find debate
+    Debate.findById(req.params.id, async (err, debate) => {
+        if (err) {
+            res.status(404);
+            res.send("Debate not found");
+        }
+
+        // create side
         const newSide = new Side({
             name: req.body.name,
             votes: []
         });
         await newSide.save();
-        res.json({
-            id: newSide._id
-        });
-    }
-    catch{
-        res.status(400);
-        res.send("Failed");
-    }
+
+        // add side to debate
+        debate.sides.push(newSide);
+        debate.save();
+
+        res.send("Success");
+    })
+
 });
 
 module.exports = router;
